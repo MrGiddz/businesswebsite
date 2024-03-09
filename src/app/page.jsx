46e20b5bec99b1a550b8200516602adcd9b78d5a
mrgiddz <mideolaniyi@outlook.com>
2024-03-09 13:11:41 +0100

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -17,7 +17,8 @@ export default function Home() {
       >
         <FadeIn className="max-w-3xl">
           <h1 className="font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl">
-            Transforming Tomorrow
+            Transforming Tomorrow: Elevate Your World with Cutting-Edge
+            Solutions
           </h1>
           <p className="mt-6 text-xl text-neutral-600">
             At the intersection of design and technology, our team thrives
